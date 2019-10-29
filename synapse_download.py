4666d09db2490148128fda422afeff3305750143
leboralli <leboralli@gmail.com>
2019-10-29 15:24:43 -0300

    Arrumando o looping

--- synapse_download.py
+++ synapse_download.py
@@ -1,5 +1,5 @@
 import synapseclient
-import shutil, os
+import shutil
 syn = synapseclient.Synapse()
 syn.login('leboralli','Eriol0284*')
 
@@ -7,13 +7,14 @@ syn.login('leboralli','Eriol0284*')
 #controle
 # samples = ['syn8239554', 'syn8239555', 'syn8231270', 'syn8231271',
 #             'syn8230808', 'syn8230810', 'syn8229813', 'syn8229817']
-samples_file = open("/home/boralli/workdir/data/list_samples.txt", "r")
-
+# samples_file = open("/home/boralli/workdir/data/list_samples.txt", "r")
+# samples_read = samples_file.read()
  # Obtain a pointer and download the data by loop
-for i in samples_file:
+with open("/home/boralli/workdir/data/list_samples.txt", "r") as f:
+    for line in f:
     # syn8238085 = syn.get(entity='syn8238085')
-    print (i)
-    i = syn.get(entity = str(i))
- # Get the path to the local copy of the data file
-    filepath = i.path
-    shutil.move(filepath, "/home/boralli/workdir/data")
+        print (line)
+        i = syn.get(entity = line)
+     # Get the path to the local copy of the data file
+        filepath = i.path
+        shutil.move(filepath, "/home/boralli/workdir/data")
