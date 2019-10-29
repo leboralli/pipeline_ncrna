ee4bc61889689a910c9f5bfceef159d8c0c93da8
leboralli <leboralli@gmail.com>
2019-10-29 15:31:43 -0300

    Tentando...

--- synapse_download.py
+++ synapse_download.py
@@ -14,7 +14,7 @@ with open("/home/boralli/workdir/data/list_samples.txt", "r") as f:
     for line in f:
     # syn8238085 = syn.get(entity='syn8238085')
         print (line)
-        i = syn.get(entity = line)
+        i = syn.get(str(line))
      # Get the path to the local copy of the data file
         filepath = i.path
         shutil.move(filepath, "/home/boralli/workdir/data")
