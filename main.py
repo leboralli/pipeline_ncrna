06a1efb6ea7ccafc9a5906e5276487fccb7bb005
leboralli <leboralli@gmail.com>
2020-02-17 12:00:35 -0500

    testando

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ import get_fastq as GF
 star_directory = '/homelocal/boralli/workdir/pipeline_v3/STAR/output/'
 # print (cf.SAMPLES)
 for smp in cf.SAMPLES:
-    os.system('ls ' str(star_directory) + str(smp))
+    os.system('ls ' + str(star_directory) + str(smp))
