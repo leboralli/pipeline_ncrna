f3fcbcfb6839ed8842394563e5f93f91336e1568
leboralli <leboralli@gmail.com>
2020-03-26 12:08:13 -0300

    Tentando criar um meio de remover arquivos extras

---
+++ cleaner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+####################################
+# Script para remover arquivos temporários
+# Na pasta de output do star
+# Autor: Leandro Augusto Freire boralli
+####################################
