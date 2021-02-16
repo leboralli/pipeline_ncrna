34971f939ee95f878766153d497a1a7079c1e9b3
Leandro Boralli <leboralli@gmail.com>
2021-02-16 20:16:55 -0300

    tentando

--- script_catFiles.py
+++ script_catFiles.py
@@ -1,10 +1,11 @@
 
-import os
+import os, re
 import config.py
 
 data = DATA_DIR
 lista = os.listdir(data)
 
+r = re.compile("R\d{4}")
 teste = []
 for i in lista:
     print (i)
@@ -12,3 +13,6 @@ for i in lista:
     teste.append(chars.stripe())
 
 print (teste)
+teste2 = list(filter(r.match, teste))
+
+print (teste2)
