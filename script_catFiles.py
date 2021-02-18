3cd5cdbe978f13f79c11f3c5cd1844fe6a535429
Leandro Boralli <leboralli@gmail.com>
2021-02-18 13:14:25 -0300

    testando

--- script_catFiles.py
+++ script_catFiles.py
@@ -16,15 +16,7 @@ lista = os.listdir(data)
 
 r = re.compile(r"^(R\d{4})")
 samples_lista = [x for x in lista if re.search(r, x)] #usando lambda
-print (samples_lista)
 # print (samples_lista)
-# teste = []
-# for i in lista:
-#     print (i)
-#
-#     teste.append(chars.stripe())
 
-# print (teste)
-# teste2 = list(filter(r.match, teste))
-
-# print (teste2)
+teste = samples_lista[0]
+print (teste)
