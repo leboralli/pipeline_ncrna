f7501e848fd2741f4e066578ad9c0de3f687a690
Leandro Boralli <leboralli@gmail.com>
2021-02-19 15:01:17 -0300

    finalizado

--- script_catMultipleFiles.sh
+++ script_catMultipleFiles.sh
@@ -10,8 +10,8 @@ echo "Numero de amostras = $IDs"
 
 for i in $@; do
   echo "Amostra: $i"
-  # for n in $(ls | grep $i | grep R1); do cat $i >> $i_allLanes_R1.fastq.gz; done
-  # for n in $(ls | grep $i | grep R2); do cat $i >> $i_allLanes_R1.fastq.gz; done
-  echo "$i"_allLanes_R1
-  echo "$i"_allLanes_R2
+  for n in $(ls | grep $i | grep R2); do cat $n >> "$n"_allLanes_R1.fastq.gz; done
+  for n in $(ls | grep $i | grep R1); do cat $n >> "$n"_allLanes_R1.fastq.gz; done
+  # echo "$i"_allLanes_R1
+  # echo "$i"_allLanes_R2
 done
