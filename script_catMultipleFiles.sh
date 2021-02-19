bba69688357889e039e6ab4d45cb2b89ac754fd2
Leandro Boralli <leboralli@gmail.com>
2021-02-19 14:37:18 -0300

    avaliando a funcionalidade

--- script_catMultipleFiles.sh
+++ script_catMultipleFiles.sh
@@ -12,6 +12,6 @@ for i in $IDs; do
   echo "Amostra: $i"
   # for n in $(ls | grep $i | grep R1); do cat $i >> $i_allLanes_R1.fastq.gz; done
   # for n in $(ls | grep $i | grep R2); do cat $i >> $i_allLanes_R1.fastq.gz; done
-  for n in $(ls | grep $i | grep R1); do echo $i_allLanes_R1 ; done
-  for n in $(ls | grep $i | grep R2); do echo $i_allLanes_R2 ; done  
+  echo $i_allLanes_R1
+  echo $i_allLanes_R2   
 done
